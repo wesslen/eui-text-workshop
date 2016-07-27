c1882217740d3db193631a969c6d26dbe036c526
Ryan Wesslen <rwesslen@gmail.com>
2016-07-27 12:38:03 -0400

    Add libraries

---
+++ libraries.R
@@ -0,0 +1,6 @@
+
+packages <- c('reshape','servr','grid','LDAvis','cvTools','topicsmodels','dplyr','quanteda','glmnet','ggplot2','rmarkdown','doMC','ggmap','ggrepel')
+
+for (i in packages){
+  install.packages(i)
+}
